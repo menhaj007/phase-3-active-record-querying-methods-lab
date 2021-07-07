f680d351aaec00eb339f19c1923055f8cfbae3f3
menhaj <menhaj007@yahoo.com>
2021-07-07 05:49:51 -0400

    commit tree cc622c6d67eaba32310825f409595a1d4a8f87d1

---
+++ app/show_controller.rb
@@ -0,0 +1,29 @@
+# def highest_rating
+#   Show.maximum(:rating)
+# end
+#
+# def most_popular_show:
+#   max = Show.maximum(:rating)
+#   show = Show.where("rating == ?", max)
+#   show
+# end
+#
+# def lowest_rating
+#   Show.minimum(:rating)
+# end
+# def least_popular_show
+#   min = Show.minimum(:rating)
+#   show = Show.where("rating ==?", min)
+#   show
+# end
+# def ratings_sum
+#   Show.sum(:rating)
+# end
+#
+# def popular_shows
+#   Show.where(":rating > 5")
+# end
+#
+# def shows_by_alphabetical_order
+#   Show.order(:name)
+# end
